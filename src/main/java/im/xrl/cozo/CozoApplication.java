adb305178018355017b0c00b52cff3c2a8dbb6dd
wpy030414 <penyoofficial@outlook.com>
2025-01-29 09:41:36 +0800

    feat: commit very firstly

---
+++ src/main/java/im/xrl/cozo/CozoApplication.java
@@ -0,0 +1,13 @@
+package im.xrl.cozo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CozoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CozoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/im/xrl/cozo/CozoApplicationTests.java
@@ -0,0 +1,13 @@
+package im.xrl.cozo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CozoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
